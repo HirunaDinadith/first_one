f1f903f41432db9674d5eb58e8f49167a90eec91
Hiruna Dinadith <hirunadinadith24@gmail.com>
2021-12-14 15:45:20 +0530

    this is mt first commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.sidebar.selectbox('Options', options)
 st.title("Hello world")
 st.write("This is my first web hosted application")
 
-video_file = open('covid.mp4', 'rb')
+video_file = open('Covid speading.mp4', 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
